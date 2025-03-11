78df0273da6bc1c59ae50e62afa81700c04c2b6a
montanari2019 <ikaro.montanari31@gmail.com>
2025-03-11 15:43:45 -0400

    ïmplementando configuracoes inicias de banco e documentacao issue #1

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsuariosModule } from './usuarios/usuarios.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [UsuariosModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.use(helmet());
+  const config = new DocumentBuilder()
+    .setTitle('Bereia Livrarias')
+    .setDescription('The Bereia Livrarias API description')
+    .setVersion('1.0')
+    .addTag('Default')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory);
+  console.log(`Servidor rodando na porta: ${String(process.env.PORT)}`);
+  await app.listen(Number(process.env.PORT));
 }
 bootstrap();
